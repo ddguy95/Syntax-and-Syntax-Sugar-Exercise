eee5cd87264456c63de92149c01f3f3bb51e4eaa
derrickdrye <dryd95@hotmail.com>
2023-06-22 14:24:31 -0500

    Completed Syntax Exercise)

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4;
+            string response;
+         if  (answer < 9)
+            {
+                response = answer + "is less than nine";
+            }
+         else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+         //Console.WriteLine(response);
+            /*-------------------------------------------------------------*/
+
+            //Inferred Typing
+
+            var answera = 4;
+            var responsea = "";
+            if (answera < 9)
+            {
+                responsea = ($"{answera} is less than nine");
+            }
+            else
+            {
+                responsea = ($"{answera} is greater than or equal to nine");
+            }
+         //Console.WriteLine(responsea);
+            /*---------------------------------------------------------------*/
+
+            // Ternary
+
+            var answerb = 4;
+            var responseb = "";
+
+            responseb = (answerb < 9) ? ($"{answerb} is less than nine") : ($"{answerb} greater than or equal to nine");
+            
+            Console.WriteLine(responseb);
+
+
+
         }
     }
 }
